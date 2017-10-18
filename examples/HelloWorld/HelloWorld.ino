ed48d1fd979efd58e0f64ef8c75ac6b167c8bfc7
Boris4K <boris.kimelman@gmail.com>
2017-10-18 10:18:55 +0300

    Add files via upload

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -1,13 +1,14 @@
-#include "LCD_1602_HEB.h"
+#include <LCD_1602_HEB.h>
 
 LCD_1602_HEB lcd(0x27, 16, 2);
+//some displays are 0x3F
 
 void setup()
 {
   lcd.init();                      // initialize the lcd
   // Print a message to the LCD.
   lcd.backlight();
-  lcd.setCursor(2, 0);
+  lcd.setCursor(4, 0);
   lcd.print(L"םלוע םולש");
 }
 
